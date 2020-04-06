6e1d66c6f8a69c0b0fd3bc329483623ecadecd21
gr8geek <pratyush9000@gmail.com>
2020-04-06 15:11:01 +0530

    Made few changes

--- dijkstras.java
+++ dijkstras.java
@@ -34,16 +34,40 @@ public static void main(String args[]){
     { Integer.MAX_VALUE, Integer.MAX_VALUE, 2, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, 6, 7, Integer.MAX_VALUE } };
     PriorityQueue<Node> q = new PriorityQueue<Node>(9, new comp());
     HashMap<Integer,Node> m = new HashMap<Integer,Node>();
-    //crating a mapping between the vertex number and the Node object
-    for(int j=0;j<9;j++){
-        
-    }
+    
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the source and destination");
     int source = sc.nextInt();
+    int dest = dc.nextInt();
+    q.add(new Node(source, 0, null));
+    m.put(source, q.peek());
+    for(int j=0;j<9;j++){
+        if(j!=source){
+        Node x = new Node(j, Integer.MAX_VALUE, null);
+        q.add(x);
+        m.put(j,x);
+    }
+        
+    }
+    HashSet<Node> visited = new HashSet<Node>();
+    int min = 0;
+    visited.add(m.get(source));
      while(){
-         
-
+         Node t = q.peek();
+         Node p = null;
+         int tem[] =graph[t.name];
+         for(int j=0;j<tem.length;j++){
+             Node x = m.get(j);
+            if(!visited.contains(m.get(j)) && x.v<min){
+                min = x.v;
+                p = x;
+            }
+         }
+         p.prev = t.name;
+         if(p.v == Integer.MAX_VALUE)
+            p.v = 0;
+        if(p.v!= Integer.MAX_VALUE)
+        p.v+=t.v;
     }
 
 }
