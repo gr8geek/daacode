7a04d7640efe89b38a9910092f90c584c2b25c47
gr8geek <pratyush9000@gmail.com>
2020-04-19 09:30:01 +0530

    Changes in bellman ford

--- bellmanford.java
+++ bellmanford.java
@@ -55,7 +55,7 @@ public class bellmanford{
             int cost=sc.nextInt();
             g.addEdge(u, v, cost);
         }
-        int v=bellmanfordfun(Graph g);
+        int v=bellmanfordfun(g);
         
     }
     public static int bellmanfordfun(Graph g){
